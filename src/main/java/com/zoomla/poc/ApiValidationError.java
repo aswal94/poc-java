7f2c80205804ae80a74b7e8d9cd6538987836135
aswal94 <aswal.amit94@gmail.com>
2021-03-20 14:04:40 +0530

    Add ValidationError

---
+++ src/main/java/com/zoomla/poc/ApiValidationError.java
@@ -0,0 +1,13 @@
+package com.zoomla.poc;
+
+public class ApiValidationError extends ApiSubError{
+    private String object;
+    private String field;
+    private Object rejectedValue;
+    private String message;
+
+    ApiValidationError(String object, String message){
+        this.object = object;
+        this.message = message;
+    }
+}
